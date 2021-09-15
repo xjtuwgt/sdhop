2674a73e5529900bb57690688ba5bbb7118e23f5
guangtao.wang <guangtao.wang@jd.com>
2021-09-15 16:02:08 -0700

    Initial commit

--- hotpotqa_randsearch.py
+++ hotpotqa_randsearch.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Required parameters
     parser.add_argument("--rand_seed", type=int, default=1000)
-    parser.add_argument("--task_num", type=int, default=5)
+    parser.add_argument("--task_num", type=int, default=1)
     args = parser.parse_args()
     generate_random_search_bash(task_num=args.task_num, seed=args.rand_seed)
