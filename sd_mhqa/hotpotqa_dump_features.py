49514a8a9b840f6ff6b6e2a6916e7f1131d1710b
guangtao.wang <guangtao.wang@jd.com>
2021-09-15 18:08:12 -0700

    Initial commit

--- sd_mhqa/hotpotqa_dump_features.py
+++ sd_mhqa/hotpotqa_dump_features.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import argparse
 from sd_mhqa.hotpotqa_data_utils import hotpot_answer_tokenizer
 from os.path import join
