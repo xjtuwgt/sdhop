3650f3246046bd8b01476fe0ad155854cf06eca6
guangtao.wang <guangtao.wang@jd.com>
2021-09-23 11:20:05 -0700

    first commit

--- hyperparatuner/sentdrop_randomsearch.py
+++ hyperparatuner/sentdrop_randomsearch.py
@@ -48,7 +48,7 @@ def HypeParameterSpace():
     learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [3e-5, 2e-5, 1e-5, 1e-5]} #3e-5, 5e-5, 1e-4, 1.5e-4
     layer_wise_lr_decay = {'name': 'layer_wise_lr_decay', 'type': 'choice', 'values': [0.9]}
     gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [1]}
-    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [15]} ##
+    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [10, 15]} ##
     drop_prob = {'name': 'drop_prob', 'type': 'choice', 'values': [0.05]}
     num_train_epochs = {'name': 'num_train_epochs', 'type': 'choice', 'values': [15]}
     sent_drop_ratio = {'name': 'sent_drop_ratio', 'type': 'choice', 'values': [0.1, 0.15, 0.2]}
