8e4aafe59f1b05c4989fd97ef0b77a5942ba5645
guangtao.wang <guangtao.wang@jd.com>
2021-09-24 17:53:58 -0700

    first commit

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,9 +1,11 @@
 from utils.ioutils import load_gz_file
-
+import numpy as np
 
 def length_analysis(data_file_name: str):
     examples = load_gz_file(file_name=data_file_name)
     num_sent_list = []
     for example in examples:
-        num_sent_list.append(example.sent_names)
-        print(example.sent_names)
+        num_sent_list.append(len(example.sent_names))
+        print(len(example.sent_names))
+    num_array = np.array(num_sent_list)
+    return num_array
