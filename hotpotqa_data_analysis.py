51709724b412e70a34a0ad828cac70000589e73e
guangtao.wang <guangtao.wang@jd.com>
2021-10-02 12:13:01 -0700

    first commit

--- hotpotqa_data_analysis.py
+++ hotpotqa_data_analysis.py
@@ -1,4 +1,4 @@
-from utils.data_utils import length_analysis
+from utils.data_utils import length_analysis, support_fact_analysis
 from envs import DATASET_FOLDER
 from os.path import join
 import numpy as np
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     folder_name = 'data_feat/train'
     train_file_name = 'cached_long_low_hotpotqa_tokenized_examples_electra.pkl.gz'
     data_file_name = join(DATASET_FOLDER, folder_name, train_file_name)
-    len_array = length_analysis(data_file_name)
+    # len_array = length_analysis(data_file_name)
+    len_array = support_fact_analysis(data_file_name)
 
     print('Min: {}\nMax: {}\nMean: {}\nP25: {}\nP50: {}\nP75: {}'.format(np.min(len_array), np.max(len_array), np.mean(len_array),
                     np.percentile(len_array, 25), np.percentile(len_array, 50), np.percentile(len_array, 75)))
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -8,3 +8,11 @@ def length_analysis(data_file_name: str):
         num_sent_list.append(len(example.sent_names))
     num_array = np.array(num_sent_list)
     return num_array
+
+def support_fact_analysis(data_file_name: str):
+    examples = load_gz_file(file_name=data_file_name)
+    num_supp_fact_list = []
+    for example in examples:
+        num_supp_fact_list.append(len(example.sup_fact_id))
+    num_array = np.array(num_supp_fact_list)
+    return num_array
